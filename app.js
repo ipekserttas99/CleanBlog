e7685bf3a345e8d81f252e2cd06953d3787cd1cd
İpek Serttaş <ipek_serttas99@hotmail.com>
2022-08-24 21:44:30 +0300

    bölüm 2.

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description"
-  }
-  res.send(photo)
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/post', (req, res) => {
+  res.render('post')
+})
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
 })
 
 const port = 3000;
